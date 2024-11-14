957aba9941129facc2086392ddaaa86df23d1b1d
OvanK <ovankurniawan04@gmail.com>
2024-11-14 22:34:24 +0700

    detail screen
    w

--- candi_ovan/lib/screens/detail_screen.dart
+++ candi_ovan/lib/screens/detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:candi_ovan/models/candi.dart';
 
@@ -98,7 +99,7 @@ class DetailScreen extends StatelessWidget {
                     const SizedBox(
                       width: 70,
                       child: Text(
-                        'Lokasi',
+                        'Dibangun',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
@@ -117,7 +118,7 @@ class DetailScreen extends StatelessWidget {
                     const SizedBox(
                       width: 70,
                       child: Text(
-                        'Lokasi',
+                        'Tipe',
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
@@ -133,8 +134,91 @@ class DetailScreen extends StatelessWidget {
                 ),
                 const SizedBox(
                   height: 16,
-                )
+                ),
                 //Info Bawah
+                const Text(
+                  'Deskripsi',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(candi.description)
+              ],
+            ),
+          ),
+          // Detail Galeri
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 8,
+                ),
+                Divider(
+                  color: Colors.deepPurple.shade100,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text(
+                  'Galeri',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: candi.imageUrls.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 8),
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              border: Border.all(
+                                color: Colors.deepPurple.shade100,
+                                width: 2,
+                              ),
+                            ),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: CachedNetworkImage(
+                                imageUrl: candi.imageUrls[index],
+                                height: 120,
+                                width: 120,
+                                fit: BoxFit.cover,
+                                placeholder: (context, url) => Container(
+                                  width: 120,
+                                  height: 120,
+                                  color: Colors.deepPurple[50],
+                                ),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                const Text(
+                  'Tap untuk zoom',
+                  style: TextStyle(fontSize: 12, color: Colors.black54),
+                ),
               ],
             ),
           ),
